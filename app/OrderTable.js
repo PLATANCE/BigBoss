4f1f8ff4885082862846f348a60fe8cd0f16d2b7
Allen Lee <pineoc@naver.com>
2016-09-10 19:46:54 +0900

    testing table on app/orderTable

--- app/OrderTable.js
+++ app/OrderTable.js
@@ -1,27 +1,78 @@
+import React from 'react';
 import {Component} from 'react';
+import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 
 class OrderTable extends Component {
+  products = [{
+    orderNum: 1,
+    userNum: "1234",
+    userName: "aa",
+    address: "강남구 논현동 122-8",
+    menu: "a,b,c",
+    time: "16:00 - 16:30"
+  },{
+    id: 2,
+    name: "1235",
+    UserName: "a",
+    address: "강남구 논현동 122-8",
+    menu: "a,b",
+    time: "17:00 - 17:30"
+  }];
+  setProduct(data) {
+    this.products = data;
+  }
+  getProduct() {
+    return this.products
+  }
 
+  renderHead() {
+    return (
+      <tr>
+        <td>head1</td>
+        <td>head2</td>
+        <td>head3</td>
+        <td>head4</td>
+        <td>head5</td>
+      </tr>
+    )
+  }
   renderRow() {
     return (
       <tr>
         <td>1</td>
         <td>2</td>
         <td>3</td>
+        <td>4</td>
+        <td>5</td>
       </tr>
     )
   }
 
   render() {
     return (
-      <table>
-        <thead />
-        <tbody>
-          {this.renderRow()}
-          {this.renderRow()}
-          {this.renderRow()}
-        </tbody>
-      </table>
+      <div>
+        <table>
+          <thead>
+          {this.renderHead()}
+          </thead>
+          <tbody>
+            {this.renderRow()}
+            {this.renderRow()}
+            {this.renderRow()}
+            {this.renderRow()}
+            {this.renderRow()}
+          </tbody>
+        </table>
+        <BootstrapTable data={this.getProduct()} search={true}>
+          <TableHeaderColumn dataField="orderNum" isKey={true} dataSort={true} width="120">주문 No.</TableHeaderColumn>
+          <TableHeaderColumn dataField="userNum" dataSort={true} width="120">회원번호</TableHeaderColumn>
+          <TableHeaderColumn dataField="userName" dataSort={true}>회원이름</TableHeaderColumn>
+          <TableHeaderColumn dataField="address">주소</TableHeaderColumn>
+          <TableHeaderColumn dataField="menu">메뉴</TableHeaderColumn>
+          <TableHeaderColumn dataField="time" dataSort={true} width="150">시간대</TableHeaderColumn>
+          <TableHeaderColumn>관리</TableHeaderColumn>
+        </BootstrapTable>
+      </div>
     )
   }
 }
